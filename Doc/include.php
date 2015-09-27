75528a3a02910acd9019ec84d0c24e22b7f3ab13
James Hazard <james.g.hazard@gmail.com>
2015-09-27 17:24:29 +0200

    does link work on hosted site?

--- Doc/include.php
+++ Doc/include.php
@@ -8,11 +8,12 @@ The top level expressed here is merely single letters:<ol><li>
 
 "U" is intended to mean something like "you" and has info about persons and places.<li>
 
-  "W" is intended to denote a www. website.  A person who has a domain could make files using that name and reasonably expect that others would not step on that name.Here, we present only "Wx" because the real owners of the domains have not "claimed" them. Because domain names are all lowercase, these names are expressed in lower case.  That, too, is just a convention.  (Because of variations in file systems, we can't ever have two files where the only difference is upper/lower case.  OSX and maybe other systems don't distinguish.)<li>
+  "W" is intended to denote a www. website.  A person who has a domain could make files using that name and reasonably expect that others would not step on that name. Here, we present only "Wx" because the real owners of the domains have not "claimed" them. Because domain names are all lowercase, these names are expressed in lower case.  That, too, is just a convention.  (Because of variations in file systems, we can't ever have two files where the only difference is upper/lower case.  OSX and maybe other systems don't distinguish.)<li>
 
 "Z" is at the end of the alphabet and a place to put little bits of infrastructure text.  Frames for lists of paragraphs, etc.  The Z/Agt/... is probably misplaced - since it is not really general purpose, but is instead only for English language (American-ish) agreements.  Not sure where to put it yet.<li>
 
   "ZZZ" is for text that helps define the website.  Currently, the file there is used to define the landing page of the site.  It is arguable whether it should be here (in Doc/) or not. The argument for is that those are pages, too, and can be programmed the same way.  The argument against is that there is a difference between the way the site is presented and works, and its "content."
 
 </ol>
+A place to start might be: <a href="index.php?action=list&file=Dx/Acme/">Acme</a>.
  
