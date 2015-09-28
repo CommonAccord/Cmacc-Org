dbb3bc3a829e5f0ac9dc09af125c46eb16b60dab
James Hazard <james.g.hazard@gmail.com>
2015-09-28 23:06:17 +0200

    making the intro text a bit more compact

--- Doc/include.php
+++ Doc/include.php
@@ -1,14 +1,15 @@
+<span style="line-height: 120%;">
 Much of "the law" is a set of naming conventions for shared ideas. So is the web.  This site attempts to establish a pattern of names.  As with everything in CommonAccord, it is not exclusive, and likely not yet quite right.  It is "a question posed in the form of an answer."  <br>
 
 The top level expressed here is merely single letters:<ol><li>
 
-"D" is intended for your deals.  These will NOT be shared publicly, of course.  We use "Dx" here because these aren't real deals.<li>
+  "D" is a place intended for your deals.  You will NOT share these publicly, of course. We use "Dx" here because the examples aren't real deals.<li>
 
-  "P" is reserved for your private, preferred, proprietary materials.  You will want some forms, clauses, instructions, etc.<li>
+  "P" is reserved for your private, preferred, proprietary materials.  You will want some place to keep your private forms, clauses, instructions, etc.<li>
 
-"U" is intended to mean something like "you" and has info about persons and places.<li>
+"U" is intended to mean something like "you" and has info about persons and places.  This obviously should tie in with identity management systems, maps, etc. The examples are placeholders designed to show that it can be done, not that it has been done.<li>
 
-  "W" is intended to denote a www. website.  A person who has a domain could make files using that name and reasonably expect that others would not step on that name. Here, we present only "Wx" because the real owners of the domains have not "claimed" them. Because domain names are all lowercase, these names are expressed in lower case.  That, too, is just a convention.  (Because of variations in file systems, we can't ever have two files where the only difference is upper/lower case.  OSX and maybe other systems don't distinguish.)<li>
+"W" is intended to denote a www. website.  A person who has a domain could make files using that name and reasonably expect that others would not step on that name. Here, we present only "Wx" because the real owners of the domains have not "claimed" them. Because domain names are all lowercase, these names are expressed in lower case.  That, too, is just a convention.  (Because of variations in file systems, we can't ever have two files where the only difference is upper/lower case.  OSX and maybe other systems don't distinguish.)<li>
 
 "Z" is at the end of the alphabet and a place to put little bits of infrastructure text.  Frames for lists of paragraphs, etc.  The Z/Agt/... is probably misplaced - since it is not really general purpose, but is instead only for English language (American-ish) agreements.  Not sure where to put it yet.<li>
 
