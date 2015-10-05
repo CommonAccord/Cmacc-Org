57fcc01686e1d9640f8a5d15272b2155d35338bc
James Hazard <james.g.hazard@gmail.com>
2015-10-05 10:39:52 +0200

    fixing links to Sage etc.

--- Doc/Dx/Alice/include.php
+++ Doc/Dx/Alice/include.php
@@ -1,8 +1,10 @@
-Alice is a fictive person.  Her name comes from medical privacy circles, where she is a fictive patient.  She usually has to deal with fictive Bob.  Alice has also become well-known to the UMA-Legal working group - e.g. at the <a href="https://github.com/KantaraInitiative/wg-uma/wiki">wiki</a>.
+Alice is a fictive person.  Her name comes from medical privacy circles, where she is a fictive patient.  She usually has to deal with fictive Bob.  Alice has also become well-known to the UMA-Legal working group - e.g. at the <a href="https://github.com/KantaraInitiative/wg-uma/wiki">wiki</a> and <a href="http://cmacc-uma.herokuapp.com">cmacc-uma</a>.
 
-The thought here is to create a sampling of documents that Alice might be confronted with.  Documents that others usually prepare and which she is poorly situated to negotiate because of lack of time, clout, competence, or interest.  I.e., Alice is in the situation of most consumers most of the time in most of their dealings.  The notion is to provide her (and everyone else) with sets of documents that protect her reasonably well.  So she can suggest to her suppliers that they use these terms.  And if not, why not?
+  The thought here is to create a sampling of documents that Alice might be confronted with.  Documents that others usually prepare and which she is poorly situated to negotiate because of lack of time, clout, competence, or interest.  Alice is in the situation of most consumers most of the time in most of their dealings.  The notion is to provide her (and everyone else) with sets of documents that protect her reasonably well.  She can suggest to her suppliers that they use these terms and, if they decline, can ask them to show how their terms differ.
 
-At the moment, we are just making a list.  The action is likely to take place at an UMA-specific repo such as reflected <a href="http://cmacc-uma.herokuapp.com">there</a>.
+  Alice can benefit from the work of <a href="http://genomicsandhealth.org">the GA4GH</a> and <a href="http://sagebase.org">Sage Bionetworks</a>.
+
+At the moment, we are mostly just making a list.
 
 
  
