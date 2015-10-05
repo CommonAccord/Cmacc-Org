39c81d91b9edb74f996229fa84e08295cb3410ab
James Hazard <james.g.hazard@gmail.com>
2015-10-05 23:58:00 +0200

    Adding ROI - Adrian

---
+++ Doc/GHx/KantaraInitiative/ROI/include.php
@@ -0,0 +1,1 @@
+Working with Adrian Gropper to create a solution for medical records.  Working out the "swimlanes" and populating with text stubs.  Will then tie in with standardized text solutions.
---
+++ Doc/GHx/KantaraInitiative/ROI/steps/include.php
@@ -0,0 +1,1 @@
+Here are the steps in a sample transaction, per Adrian.  Step 4 has text from NY Presbyterian.  To be filled out. 
