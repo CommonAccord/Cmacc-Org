4dbdc3a2588d4f3394695f705a0dada8eb4e95c2
James Hazard <james.g.hazard@gmail.com>
2015-10-12 09:21:57 +0200

    a little more introduction to Model Clauses example

--- Doc/Dx/Acme_UK/01-EU-US-DataTransfer/include.php
+++ Doc/Dx/Acme_UK/01-EU-US-DataTransfer/include.php
@@ -1,4 +1,6 @@
-The Model Clauses are demonstrated as a form for Acme France and another one for Acme UK.  Both use a data sheet for Acme Incorporated, and each selects its appropriate language and (in the case of UK) a Member State overlay.
+The Model Clauses are demonstrated as a form for Acme France, Acme Deutschland, and Acme UK.  They use a data sheet for Acme Incorporated, and each selects its appropriate language and (in the case of UK) a Member State overlay.
 <br>
-<a href="index.php?action=source&file=Dx/Acme_FR/11-EU-US-DataTransfer/Doc_v0.md">Acme France</a><br>
+<a href="index.php?action=source&file=Dx/Acme_DE/01-EU-US-DataTransfer/Doc_v0.md">Acme DE</a>
+<br>
+<a href="index.php?action=source&file=Dx/Acme_FR/11-EU-US-DataTransfer/Doc_v0.md">Acme FR</a>
 
