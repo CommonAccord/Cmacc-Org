d7eb70812a8dac5e35e9fdec7d5594ad88c10635
James Hazard <james.g.hazard@gmail.com>
2015-10-12 09:28:16 +0200

    listing the three uses DE, FR, UK

--- Doc/Dx/Acme/09-EU-US-DataTransfer/include.php
+++ Doc/Dx/Acme/09-EU-US-DataTransfer/include.php
@@ -1,1 +1,9 @@
-This is a term sheet used in the agreements by Acme DE, FR, UK.  They are filed in the subs' folders.  You can find all of them by going to this file, then clicking on "Used By".  (It's a "graph," so no more lost documents.)
+This is a term sheet used in the agreements by Acme DE, FR, UK.  They are filed in the subs' folders.
+<br>
+<a href="index.php?action=source&file=Dx/Acme_DE/01-EU-US-DataTransfer/Doc_v0.md">Acme DE</a>
+<br>
+<a href="index.php?action=source&file=Dx/Acme_FR/11-EU-US-DataTransfer/Doc_v0.md">Acme FR</a>
+<br>
+<a href="index.php?action=source&file=Dx/Acme_UK/01-EU-US-DataTransfer/Doc_v0.md">Acme UK</a>
+
+
