2e41d70114df14ce905aa8fac0f0a992e12b18c8
James Hazard <james.g.hazard@gmail.com>
2015-11-13 14:01:55 +0100

    3 samples, roughly in place

--- Doc/Dx/MIT/01-MaterialsTransfer/Sample_4.md
+++
@@ -1,5 +0,0 @@
-=[Dx/MIT/01-MaterialsTransfer/Sample_1.md]
-
-Material.=[U/is/Tissue/ABCD5F/re_bio_micro_kaki]
-
-P2.ResearchPurpose=characterization
---
+++ Doc/Dx/MIT/01-MaterialsTransfer/include.php
@@ -0,0 +1,14 @@
+This demo will show record-keeping for the collection and transfer of biological samples.  It will also show managed access to the information relating to the sample. 
+
+<br>
+The terms of the MTA are established in /Dx/MIT/01-MaterialsTransfer/CoverPage.md.
+<br>
+The CoverPage is used to create a specific MTA for each sample, as /Dx/MIT/01-MaterialsTransfer/Sample_1.md, et seq.
+<br>
+Each specific MTA references a file of metadata associated with the sample by a GUID. In /U/is/Tissue/ABCD5E/*.
+
+<br><br>
+The example can be deepened by reflecting the stages of the negotiation and signature, and by a succession of communications following transfer.  
+<br>
+These materials can be hypothetically interrogated by a third party authorized by the Donor or the Scientist.   
+
