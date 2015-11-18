dc05f50f022ce335936f54c9b6e93e2ab68306af
James Hazard <james.g.hazard@gmail.com>
2015-11-18 09:26:36 +0100

    explain faux hash

---
+++ Doc/S/Sandbox/HashFileNames/include.php
@@ -0,0 +1,9 @@
+Playing around with a faux hash system for file naming.  
+
+<br> 
+
+Each bit of work is saved as a file whose name is a hash of the file.  The file also contains the previous state of the file in the form of a =[2s4TiZ7dE..] link to the previous state. The new file could contain only the modified key/values (for this we need also to have a formal way of eliminating a key/value).  This would happen automatically in an Xray editing view since only the edits would be created by the user interaction.
+
+<br>
+
+Here are a few pretend hashes (number sequentially to make it easier on me).
