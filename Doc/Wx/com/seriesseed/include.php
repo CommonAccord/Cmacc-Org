ef224711c270b9269e73aebe295fe87cd6fca950
James Hazard <james.g.hazard@gmail.com>
2015-12-26 16:02:41 +0100

    Startup: Series Seed: adding the docs from earlier repo
    Was in Site-Org (and some others).  Now available in Cmacc-Org.

---
+++ Doc/Wx/com/seriesseed/include.php
@@ -0,0 +1,1 @@
+These are the Series Seed documents.  They are available at <a href="http://github.com/seriesseed">GitHub</a> and at <a href="http://seriesseed.com">seriesseed.com</a>. <br>These have been code-ified and parameterized.  Because they have been through a couple of iterations since being ported from the originals, they may differ.  So use with care (as with everything here).
