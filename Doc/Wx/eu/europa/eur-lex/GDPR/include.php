8d43bc65537212d2632a2a3c75748d1978016dfe
CommonAccord <james.g.hazard@gmail.com>
2016-08-27 16:04:09 -0400

    Adding GDPR -

---
+++ Doc/Wx/eu/europa/eur-lex/GDPR/include.php
@@ -0,0 +1,3 @@
+GDPR divided into sections and up on Github.  Available for annotation.<br>
+
+From <a href="http://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX:32016R0679&from=EN">http://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX:32016R0679&from=EN</a>
