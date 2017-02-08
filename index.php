b0c34bf17fa3b7de6b7e562c562d85baf16ded8a
CommonAccord <james.g.hazard@gmail.com>
2017-02-08 09:33:27 -0800

    Landing.md -> Landing2.md
    Some bot keeps rewriting the landing page.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ DEFINE('ROOT' ,dirname(__FILE__) ); // Root directory of site
 
 DEFINE('URLFORREPO', 'https://github.com/CommonAccord/Cmacc-Org');	// The repo home
 
-DEFINE('LANDING_MD', 'S/About/Landing.md');	// The website home
+DEFINE('LANDING_MD', 'S/About/Landing2.md');	// The website home
 
 DEFINE('SOURCE_TAB_MESSAGE', 'Source');	// Tab
 
