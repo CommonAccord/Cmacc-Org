6f61e9cc2e853b2212c903afb34387170b1f0d6f
CommonAccord <james.g.hazard@gmail.com>
2017-02-08 16:03:50 -0800

    Removing the Edit links on the web version because some bot keeps editing the text.
    Will see if this helps.

--- vendor/library/view-tabs.php
+++ vendor/library/view-tabs.php
@@ -4,9 +4,9 @@ echo "<a href=index.php?action=list&file=><img src='" . ASSETS_PATH . "/arrowup.
 
 echo "<a href=index.php?action=source&file=" .$dir.">". SOURCE_TAB_MESSAGE."</a> &emsp; ";
 
-echo "<a href=index.php?action=edit&file=" .$dir.">".EDIT_TAB_MESSAGE."</a> &emsp; ";
+#echo "<a href=index.php?action=edit&file=" .$dir.">".EDIT_TAB_MESSAGE."</a> &emsp; ";
 
-echo "<a href=index.php?action=openedit&file=" .$dir.">". COMPLETE_TAB_MESSAGE."</a> &emsp; ";
+#echo "<a href=index.php?action=openedit&file=" .$dir.">". COMPLETE_TAB_MESSAGE."</a> &emsp; ";
 
 echo "<a href=index.php?action=ipld&file=" .$dir.">". IPLD_TAB_MESSAGE
 ."</a> &emsp; ";
