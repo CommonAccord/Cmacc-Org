88fe558312cba60dd58cfde9fca9dccacca7f197
CommonAccord <james.g.hazard@gmail.com>
2017-10-19 09:31:18 -0700

    Transnational Rules:  First cut on French version.  Some xrefs automated

--- vendor/cmacc-app/openedit-parser.pl
+++ vendor/cmacc-app/openedit-parser.pl
@@ -102,6 +102,9 @@ print "$_=\n" foreach @arr;
 
 # print "$_=<a href='#Def.$_.sec' class='definedterm'>$_</a>\n" foreach @arr;
 
+# To make a new Xnum, with a hyperlink to the Section:
+
+# print "$_.Xnum=<a href='#$_.Sec' class='xref'></a>\n" foreach @arr;
 
 # To make a new Param, with a hyperlink to the definition:
 
