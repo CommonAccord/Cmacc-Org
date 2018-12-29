56c46eff94d6f2722bee4b5629bb0d194db49aeb
jamesghazard <james.g.hazard@gmail.com>
2018-12-29 16:26:27 +0100

    JSON - adding link to Brown DISP

--- vendor/cmacc-app/view/json.php
+++ vendor/cmacc-app/view/json.php
@@ -6,7 +6,7 @@ include("$lib_path/view-tabs.php");
 ?>
 
 
-<h4>JSON source view:</h4>
+<h4>JSON source view - <a href="https://github.com/adonalsium/common_accord/blob/dev/README.md">see technical description from Brown DISP ReadMe</a>:</h4>
 
 <?php 
 $text="{<ul type='none'><li>\"data\" { <ul type='none'><li>";
