2e1a0aa3de515b6c69cb1247da5aced7873f2f09
jamesghazard <james.g.hazard@gmail.com>
2019-03-26 09:13:02 -0700

    cmacc-app - remove edit

--- vendor/cmacc-app/view-tabs.php
+++ vendor/cmacc-app/view-tabs.php
@@ -11,7 +11,7 @@ echo "<b><a href=index.php?action=doc&file=$dir>". DOC_TAB_MESSAGE
 
 echo "<a href=index.php?action=xray&file=$dir>Xray</a> ";
 
-echo "<a href=index.php?action=xEdit&file=$dir>xEdit</a> ";
+# echo "<a href=index.php?action=xEdit&file=$dir>xEdit</a> ";
 
 echo "<a href=index.php?action=visual&file=$dir> Visual</a> ";
 
@@ -23,20 +23,19 @@ echo " &emsp; Source views: ";
 
 echo "<a href=index.php?action=source&file=$dir>". SOURCE_TAB_MESSAGE."</a> ";
 
-echo "<a href=index.php?action=edit&file=$dir>".EDIT_TAB_MESSAGE."</a> ";
+# echo "<a href=index.php?action=edit&file=$dir>".EDIT_TAB_MESSAGE."</a> ";
 
-echo "<a href=index.php?action=openedit&file=$dir>". COMPLETE_TAB_MESSAGE."</a> ";
+# echo "<a href=index.php?action=openedit&file=$dir>". COMPLETE_TAB_MESSAGE."</a> ";
 
 echo "<a href=index.php?action=json&file=$dir>". "JSON"
 ."</a> ";
 
 echo "&emsp; On GitHub: ";
+
 echo "<a href=" . URLFORDOCSINREPO . $dir.">File</a> ";
 
 echo "<a href=" . URLFORREPO . "/search?utf8=✓&q=" .$dir. ">~PageRank </a>  &emsp; ";
 
-
 echo "(rare: 'ShowMe' <a href=index.php?action=showme1&file=" .$dir.">1</a>) ";
-
 
 ?>
