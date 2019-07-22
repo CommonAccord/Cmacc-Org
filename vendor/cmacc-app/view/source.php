bda6bc66a069436b97bba5c511c6bfca68f42a61
jamesghazard <james.g.hazard@gmail.com>
2019-07-22 09:50:35 +0300

    app/view/source.php - permitting comments following a link ///

--- vendor/cmacc-app/view/source.php
+++ vendor/cmacc-app/view/source.php
@@ -10,10 +10,13 @@ include("$lib_path/view-tabs.php");
 echo "<div class='container'>___<hr><table class='TFtable';>";
 foreach($contents as $n) {
         list($k, $v) = array_pad( explode ("=", $n, 2), 2, null);
-
+       
 	//for remote files via http
         if(preg_match('/\[\?(.+?)\]/', $v, $matches)) {
-                $v = "<a href=$matches[1]>$v</a>";
+	  // to allow comments in the target field.
+	 	  list($v1, $v2) = array_pad( explode ("///", $v, 2), 2, null);
+	  	  $v1=substr($v1, 2, -2);
+	  $v = "<a href=$matches[1]>$v1</a></td><td>$v2";
         }
 	//for folders (navigation)
 
@@ -24,7 +27,10 @@ foreach($contents as $n) {
 	//for local files
 
         else if(preg_match('/\[(.+?)\]/', $v, $matches)) {
-                $v = "<a href=$_SERVER[PHP_SELF]?action=source&file=$matches[1]>$v</a>";
+          // to allow comments in the target field.
+	  list($v1, $v2) = array_pad( explode ("///", $v, 2), 2, null);
+
+	  $v = "<a href=$_SERVER[PHP_SELF]?action=source&file=$matches[1]>$v1</a></td><td>$v2";
 
         }
 
