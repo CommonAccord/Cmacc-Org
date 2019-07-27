82a5eb0e1adb4764c974d5b12dcedcdc870856bb
jamesghazard <james.g.hazard@gmail.com>
2019-07-27 14:28:06 +0300

    Microsoft - Open-Use-of-Data-Agreement - first brush.

---
+++ Doc/G/Open-Use-of-Data-Agreement/build-docs.ps1
@@ -0,0 +1,2 @@
+pandoc .\O-UDA-0.1_annotated_discussion-draft.md -o .\O-UDA-0.1_annotated_discussion-draft.docx
+pandoc .\O-UDA-0.1_annotated_discussion-draft.md -o .\O-UDA-0.1_annotated_discussion-draft.pdf
