a7fd1d33832054e9dc7123c553719ea072859cf7
jamesghazard <james.g.hazard@gmail.com>
2019-10-14 18:46:25 +0200

    removing the links to editable views.

--- vendor/cmacc-app/view-tabs.php
+++ vendor/cmacc-app/view-tabs.php
@@ -6,12 +6,11 @@ echo "<a href=index.php?action=list&file=>Docs</a>/<a href=$_SERVER[PHP_SELF]?ac
 
 echo "Document views: ";
 
-echo "<b><a href=index.php?action=doc&file=$dir>". DOC_TAB_MESSAGE
-."</a></b> ";
+echo "<b><a href=index.php?action=doc&file=$dir>". DOC_TAB_MESSAGE ."</a></b> ";
 
 echo "<a href=index.php?action=xray&file=$dir>Xray</a> ";
 
-echo "<a href=index.php?action=xEdit&file=$dir>xEdit</a> ";
+# echo "<a href=index.php?action=xEdit&file=$dir>xEdit</a> ";
 
 echo "<a href=index.php?action=visual&file=$dir> Visual</a> ";
 
@@ -23,22 +22,20 @@ echo " &emsp; Source views: ";
 
 echo "<a href=index.php?action=source&file=$dir>". SOURCE_TAB_MESSAGE."</a> ";
 
-echo "<a href=index.php?action=edit&file=$dir>".EDIT_TAB_MESSAGE."</a> ";
+# echo "<a href=index.php?action=edit&file=$dir>".EDIT_TAB_MESSAGE."</a> ";
 
-echo "<a href=index.php?action=openedit&file=$dir>". COMPLETE_TAB_MESSAGE."</a> ";
+# echo "<a href=index.php?action=openedit&file=$dir>". COMPLETE_TAB_MESSAGE."</a> ";
 
-echo "<a href=index.php?action=json&file=$dir>". "JSON"
-."</a> ";
+echo "<a href=index.php?action=json&file=$dir>". "JSON(ish)" ."</a> ";
 
-echo "<a href=index.php?action=kvs&file=$dir> KVs</a> ";
+# echo "<a href=index.php?action=kvs&file=$dir> KVs</a> ";
 
 echo "&emsp; On GitHub: ";
+
 echo "<a href=" . URLFORDOCSINREPO . $dir.">File</a> ";
 
-echo "<a href=" . URLFORREPO . "/search?utf8=✓&q=" .$dir. ">~PageRank </a>  &emsp; ";
-
+echo "<a href=" . URLFORREPO . "/search?utf8=✓&q=" . $dir . ">~PageRank </a>  &emsp; ";
 
 echo "(rare: 'ShowMe' <a href=index.php?action=showme1&file=" .$dir.">1</a>) ";
-
 
 ?>
