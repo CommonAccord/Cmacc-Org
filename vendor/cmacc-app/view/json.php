b41ed9249af10f991caf7fbd0c3e11a2315338c4
jamesghazard <james.g.hazard@gmail.com>
2020-04-01 07:53:43 -0700

    JSON.php - presenting edges first

--- vendor/cmacc-app/view/json.php
+++ vendor/cmacc-app/view/json.php
@@ -9,33 +9,10 @@ include("$lib_path/view-tabs.php");
 <h4>JSON source view - <a href="https://github.com/adonalsium/common_accord/blob/dev/README.md">see technical description from Brown DISP ReadMe</a>:</h4>
 
 <?php 
-# First the Data k/v's.  :
 
-$text="{<ul type='none'><li>\"data\" { <ul type='none'><li>";
+$text="{<ul type='none'><li>\"edges\" : [ <ul type='none'><li>";
 
-foreach($contents as $n) {
-  list($k, $v) = array_pad( explode ("=", $n, 2), 2, null);
-
-if(preg_match('/\[(.+?)\]/', $v, $matches)) {
-
-}
-
-
-elseif(isset($v)) { 
-$vhtml =  htmlspecialchars(json_encode($v));
-$vhtml = str_replace('\/', '/', $vhtml);
-$vhtml = str_replace('{', '", "', $vhtml);
-$vhtml = str_replace('}', '", "', $vhtml);
-$text= $text .  "\"$k\" : $vhtml ,<br><br>"; 
-}
-  else { 
-}
-}
-$text = $text . "ENDDATA";
-$text = str_replace(",<br><br>ENDDATA", "<br>}</ul>\"edges\" : [<ul type='none'><li>", $text);
-
-
-# Then the links (edges):
+# First the links (edges):
 
 foreach($contents as $n) {
         list($k, $v) = array_pad( explode ("=", $n, 2), 2, null);
@@ -54,10 +31,39 @@ if(preg_match('/\[(.+?)\]/', $v, $matches)) {
 
 
 }
-# $text = str_replace(", <br><th height='10' style='text-align:right'></th><td width='20'></td><td></td>}", "<br><th height='10' style='text-align:right'></th><td width='20'></td><td></td>}</ul>", $text);
+
+$text = $text . "ENDEDGES";
+$text = str_replace("ENDEDGES", "<br>]</ul>\"data\" {<ul type='none'><li>", $text);
+
+
+
+
+# Then the Data k/v's.  :
+
+
+foreach($contents as $n) {
+  list($k, $v) = array_pad( explode ("=", $n, 2), 2, null);
+
+if(preg_match('/\[(.+?)\]/', $v, $matches)) {
+
+}
+
+elseif(isset($v)) { 
+$vhtml =  htmlspecialchars(json_encode($v));
+$vhtml = str_replace('\/', '/', $vhtml);
+$vhtml = str_replace('{', '", "', $vhtml);
+$vhtml = str_replace('}', '", "', $vhtml);
+$text= $text .  "\"$k\" : $vhtml ,<br><br>"; 
+}
+  else { 
+}
+}
+
+
 
 $text = $text . "QEND";
-$text = str_replace(', QEND', '<br>]</ul></ul>}', $text);
+$text = str_replace('QEND', '<br>}</ul></ul>}', $text);
+
 
 echo $text
 ?>
