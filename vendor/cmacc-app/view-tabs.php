3f80d80325c29ddc8155821fa3cdfc25f7a674fc
HazardJ <jamesghazard@gmail.com>
2020-06-20 07:11:52 -0700

    removing "Missing" tab.

--- vendor/cmacc-app/view-tabs.php
+++ vendor/cmacc-app/view-tabs.php
@@ -22,7 +22,7 @@ echo "<a href=index.php?action=source&file=$dir>". SOURCE_TAB_MESSAGE."</a> ";
 
 # echo "<a href=index.php?action=edit&file=$dir>".EDIT_TAB_MESSAGE."</a> ";
 
-echo "<a href=index.php?action=openedit&file=$dir>". COMPLETE_TAB_MESSAGE."</a> ";
+# echo "<a href=index.php?action=openedit&file=$dir>". COMPLETE_TAB_MESSAGE."</a> ";
 
 echo "<a href=index.php?action=missing&file=$dir>". "OpenParameters" ."</a> ";
 
