d996e9e61238f4a15817b945b4fd8471e4b62e50
hazardj <jamesghazard@gmail.com>
2021-12-28 21:12:29 -0800

    referencing the /image/custom.css rather than vendor/png/custom.css - perhaps references to vendor/ are prohibited on Heroku instance.

--- vendor/cmacc-app/header.php
+++ vendor/cmacc-app/header.php
@@ -4,7 +4,7 @@
 
 <title><?php echo $dir ?></title>
 <link  href="Doc/G/Z/CSS/Visual.css" rel="stylesheet" />
-<link  href="<?php echo ASSETS_PATH; ?>/custom.css" rel="stylesheet" />
+<link  href="image/custom.css" rel="stylesheet" />
 
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" />
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
