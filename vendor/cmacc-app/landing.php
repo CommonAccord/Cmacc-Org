d9f96976032e24bfb3322699750375d9b0a9ec81
hazardj <jamesghazard@gmail.com>
2022-01-08 22:00:23 -0800

    conform landing.php to the new key= enabled format

--- vendor/cmacc-app/landing.php
+++ vendor/cmacc-app/landing.php
@@ -9,7 +9,14 @@ $lib_path = LIB_PATH;
 
 $dir=LANDING_MD;
 
-$document = `perl $lib_path/parser.pl $path/$dir`;
+# Adding ability to pass a starting Key to the rendering
+
+if (strlen($keyName) < 2) { 
+  $keyName = "Model.Root";
+}
+
+$document = `perl $lib_path/parser.pl $path/$dir $keyName `;
+
 
 if (strlen($document) > 1){ 
  
