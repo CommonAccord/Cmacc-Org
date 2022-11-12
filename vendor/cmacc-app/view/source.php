df12ae98be26b29961511d8bb7eb05871dc47f34
hazardj <jamesghazard@gmail.com>
2022-11-12 14:54:54 -0800

    cmacc-app - enabling quick view of linked resources - e.g. clicking on the key "Foo." in Foo.=[Bar.md] will render as Foo.Model.Root - i.e. the default rendering of Bar.md.

--- vendor/cmacc-app/view/source.php
+++ vendor/cmacc-app/view/source.php
@@ -22,9 +22,9 @@ foreach($contents as $n) {
 
 	else if(preg_match('/\[(.+?)\\/]/', $v, $matches)) {
                 $v = "<a href=$_SERVER[PHP_SELF]?action=list&file=$matches[1]/>$v</a>";
-	}
+              	}
 
-	//for local files
+	//for local file links "[G/...]"
 
         else if(preg_match('/\[(.+?)\]/', $v, $matches)) {
           // to allow comments in the target field.
@@ -36,10 +36,20 @@ foreach($contents as $n) {
         $v = str_replace('{', '<span class="missing">{', $v);
         $v = str_replace('}', '}</span>', $v);
         echo "<tr id=$k>";
-       	$klink="<a href=$_SERVER[PHP_SELF]?action=doc&file=$dir&key=$k class='definedterm'>$k</a>" ;
-        if(isset($v)) { echo "<th height='10' width='300' style='text-align:right'>$klink</th><td width='20'></td><td>$v</td>"; }
+        # enabling hyperlinks from the key.  If "foo." then make it "foo.Model.Rool", else just use the key.
+        # The key ends in a "." and therefore we render the default content of the target object (Model.Root)
+        if((substr($k, -1)==".")){
+       	        $klink="<a href=$_SERVER[PHP_SELF]?action=doc&file=$dir&key=$k" . "Model.Root class='definedterm'>$k</a>" ;
+        }
+        # The key does not end in "." so we render the key.
+       else {        
+        $klink="<a href=$_SERVER[PHP_SELF]?action=doc&file=$dir&key=$k class='definedterm'>$k</a>" ;
+       }
+        if(isset($v)) { 
+                echo "<th height='10' width='300' style='text-align:right'>$klink</th><td width='20'></td><td>$v</td>"; }
         else { echo "<th height='10' style='text-align:right'></th><td width='20'></td><td>$k</td>"; }
         echo "</tr>";
+
 }
 
 ?>
