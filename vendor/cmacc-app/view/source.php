cb29077c7788c13118c36ffbaa52c34b5b77ea2f
hazardj <jamesghazard@gmail.com>
2024-01-17 16:56:24 -0800

    source.php - split k, v, c

--- vendor/cmacc-app/view/source.php
+++ vendor/cmacc-app/view/source.php
@@ -9,72 +9,64 @@ include("$lib_path/view-tabs.php");
 
 echo "<div><table class='TFtable'>";
 foreach($contents as $n) {
-        list($k, $v) = array_pad( explode ("=", $n, 2), 2, null);
+
+# split the line into key, value, and comment.
+        list($k, $vc) = array_pad( explode ("=", $n, 2), 2, null);
+        list($v, $c) = array_pad( explode ("//", $vc, 2), 2, null);
        
 	//for remote files via http
         if(preg_match('/\[http(.+?)\]/', $v, $matches)) {
-	  // to allow comments in the target field.
-	 	  list($v1, $v2) = array_pad( explode ("//", $v, 2), 2, null);
-	  //	to trim off the brackets  $v1=substr($v1, 1, -1);
-	  $v = "<a href=http$matches[1]>$v1</a> $v2";
+	  $vlink = "<a href=http$matches[1]>$v</a> $v2";
         }
-	//for folders (navigation)
-
+	//for local folders (navigation) [G/.../]
 	else if(preg_match('/\[(.+?)\\/]/', $v, $matches)) {
-                $v = "<a href=?" . VIEW . "=list&" . FILENAME . "=$matches[1]/>$v</a>";
+                $vlink = "<a href=?" . VIEW . "=list&" . FILENAME . "=$matches[1]/>$v</a>";
               	}
 
 	//for local file links "[G/...]"
-
         else if(preg_match('/^\[(.+?)\]/', $v, $matches)) {
-          // to allow comments in the target field.
-	  list($v1, $v2) = array_pad( explode ("//", $v, 2), 2, null);
 # formatting the value side of the table cell. Pick one of the following:
     #conventional:
-       $v = "<a href=?" . VIEW . "=s&" . FILENAME . "=$matches[1]>$v1</a> $v2";
-    # in a frame:
-	# $v = "<iframe src=?v=s&f=$matches[1] style='height:300px;width:1000px' title='Iframe Example'></iframe>";
-
+       $vlink = "<a href=?" . VIEW . "=s&" . FILENAME . "=$matches[1]>$v</a>";
+  
         }
-        else
-
-# In the VALUE, each {parameter} is a hyperlink to the parameter's content:
-        #  From GitHub Copilot:  The regex {[^}]+} matches any text that starts with { and ends with }.  The + means one or more characters.  The [^}]+ means any character other than }.  The first capture group is the text between the curly braces.
-        #  But the rest is HazardJ's duct tape.
+        else{
 
         # This gives us the {parameter} {parameter}, two times, so can build the href link.
         $v = preg_replace('{{[^}]+}', 'q1q${0}x2x${0}</a>', $v);
         # We need to be able to evaluate the content of the PHP variables, which didn't work inside the str_replace.
-        $href = "{<a href=?" . VIEW . "=d&" . FILENAME . "=$dir&" . KEYNAME . "=" ;
+        $href = "{<a href=?v=d&" . FILENAME . "=$dir&" . KEYNAME . "=" ;
         $v = str_replace('q1q{', $href, $v);
-        $v = str_replace('x2x{', ' class=variable >' , $v ) ;
+        $vlink = str_replace('x2x{', ' class=variable >' , $v ) ;
         # This is the end of HazardJ's duct tape.
- 
+        }
 #Now the KEY:
         echo "<tr id=$k>" ;
         # enabling hyperlinks from the key.  
                 # If ends in a period "." then assume it is a prefix *=(e.g. key=[node]) make it "key.r00t" to render the default content of [node]. 
                 # Else just use the key.
+       # the key has a space in it, so we don't want to make it a hyperlink.
+       if(preg_match('/\s/', $k)){
+        $klink=$k ;}
+
         # The key ends in a "." and therefore we render the default content of the target object (r00t)
-        if((substr($k, -1)==".") && (!preg_match('/\s/', $k))){
+        else if((substr($k, -1)==".") && (!preg_match('/\s/', $k))){
        	        $klink="<a class='expand' href=?" . VIEW . "=d&" . FILENAME . "=$dir&" . KEYNAME . "=$k" . "r00t >$k</a>" ;
         }
-        # The key does not end in "." so we render the key.
+        # The key ends in ":" so we render the key.
         # We will make each key into a hyperlink to the key's content. But if the key has a space in it, then we don't want to make it a hyperlink.
-        else if(preg_match('/\s/', $k)){
+        else if((substr($k, -1)==":") && (!preg_match('/\s/', $k))){
        	        $klink= $k  ;
         }
         
         # The key does not have a space in it (a browser URL mishandles spaces), so we render the key with a hyperlink to the key's content.
         else {
-       	 $klink="<a href=?" . VIEW . "=d&" . FILENAME . "=$dir&" . KEYNAME . "=$k class='definedterm'>$k</a>" ;
+     	 $klink="<a href=?" . VIEW . "=d&" . FILENAME . "=$dir&" . KEYNAME . "=$k class='definedterm'>$k</a>" ;
         }
 
        if(((strlen($k)>0) || (strlen($v)>0))) { 
 #                if(isset($v)) { 
-
-                echo "<td class='table-key-source' >$klink</td><td width='20px' align='center' valign='top'>=</td><td class='table-value-source'>$v</td>"; }
-#        else { echo "<th height='10' style='text-align:right'></th><td width='20'></td><td>$k</td>"; }
+echo "<td class='table-key-source' >$klink</td><td width='20px' align='center' valign='top'> = </td><td class='table-value-source'>$vlink $c</td>"; }
         echo "</tr>";
 
 }
