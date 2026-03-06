74b087731a3b3f94e835292f9d18aead446dc850
hazardj <jamesghazard@gmail.com>
2026-03-06 14:14:36 -0800

    Claude changes to Doc view (v=d).  Adding back in the top-of-page list of views.

--- vendor/cmacc-app/view/doc.php
+++ vendor/cmacc-app/view/doc.php
@@ -7,13 +7,13 @@ $cssspecial = `perl $lib_path/parser-head.pl $path/$dir`;
 if (strlen($cssspecial) > 5){
   $cssdoc = $cssspecial ;
   }
-/*
+
 echo "<!DOCTYPE html><head><title>$dir</title><link  href='$cssdoc' rel='stylesheet' /> <link rel='icon' href='vendor/png/CmA-Square.png'></head><body>";
 
 include("$lib_path/view-tabs.php");
 
 echo "<hr>";
-*/
+
 $lib_path = LIB_PATH;
 
 
